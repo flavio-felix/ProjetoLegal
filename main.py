3e8432b70cd200dda761b59a0c582158fca3c4b9
Flavio Felix <desenhista.felix@yahoo.com.br>
2021-09-23 20:49:40 -0300

    alteracao de codigo

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #coding: utf-8
 
 class Primeiro:
-  def func()
-    print("oi")
+    def __init__(self):
+        self.var = 0
+
+    def func(self):
+        return self.var 
+
+    def entra(self, x):
+        self.var = x
 
 p = Primeiro()
-p.func()
+p.entra(100)
+print(p.func())
