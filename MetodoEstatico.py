3b4cf8270dec5441ab809782e9e774b85d78079c
Flavio Felix <desenhista.felix@yahoo.com.br>
2021-09-24 12:32:52 -0300

    atualiza codigo

--- MetodoEstatico.py
+++ MetodoEstatico.py
@@ -1,15 +1,32 @@
-#coding: utf-8
+# -*- coding: cp1252 -*-
 
-class Primeiro:
-    def __init__(self):
-        self.var = 0
+class MEstaticos:
+    @staticmethod
+    def func1():
+        print("func1()")
+    @staticmethod
+    def func2(x, y):
+        print("Função '{}' invocada.\nParams=({}, {})".format("func2", x, y))
+    @staticmethod
+    def func3(a, b, c):
+        info = """
+        Nome da Função: {nome}
+        Quantidade de Args: {quantidade}
+        Args: {args}
+        """
+        info =info.format(
+            nome=MEstaticos.func3.__name__,
+            quantidade=MEstaticos.func3.__code__.co_argcount,
+            args=MEstaticos.func3.__code__.co_varnames
+            )
+        print(info)
 
-    def func(self):
-        return self.var 
+#    func1 = staticmethod(func1)
+#    func2 = staticmethod(func2)
+#   func3 = staticmethod(func3)
+    
 
-    def entra(self, x):
-        self.var = x
-
-p = Primeiro()
-p.entra(100)
-print(p.func())
+me = MEstaticos()
+me.func1()
+me.func2(100, 200)
+me.func3(50, 10, 15)
